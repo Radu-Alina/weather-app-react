3046915fc2d7f8d2d152d229619cfa8288c9206f
Radu-Alina <alinamomirov@gmail.com>
2022-10-29 13:37:56 +0200

    Added the html to CSS to react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import WeatherSearch from './WeatherSearch';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+    <div>
+      <small>
+        <a href="https://github.com/Radu-Alina"> Open source code </a>by <a> Alina Radu</a>
+      </small>
+    </div>
+  </StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
