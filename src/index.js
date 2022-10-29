d6257f699262564fc87d5230be9857223bc5cb98
Radu-Alina <alinamomirov@gmail.com>
2022-10-29 15:05:12 +0200

    made a correction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import WeatherSearch from './WeatherSearch';
 import reportWebVitals from './reportWebVitals';
-import axios from "axios";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
